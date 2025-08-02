c824b50947cbdacd3630e85ab10bdb67e417cc5b
hzj <hezhijian249@qq.com>
2025-08-02 22:57:02 +0800

    feat: 关闭16进制校验

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,11 @@
 
 import { defineConfig } from '@vben/eslint-config';
 
-export default defineConfig();
+export default defineConfig([
+  {
+    rules: {
+      'unicorn/numeric-separators-style': 'off',
+      'unicorn/number-literal-case': 'off',
+    },
+  },
+]);

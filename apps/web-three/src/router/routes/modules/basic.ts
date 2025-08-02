3a162bfbaaff71d6f0e5923aad135b8057ebfdef
hzj <hezhijian249@qq.com>
2025-08-02 22:56:04 +0800

    feat: 配置路由和i18n

---
+++ apps/web-three/src/router/routes/modules/basic.ts
@@ -0,0 +1,29 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+import { $t } from '#/locales';
+
+const routes: RouteRecordRaw[] = [
+  {
+    meta: {
+      icon: 'ic:baseline-view-in-ar',
+      keepAlive: true,
+      order: 1000,
+      title: $t('page.basic.title'),
+    },
+    name: 'Basic',
+    path: '/basic',
+    children: [
+      {
+        meta: {
+          title: $t('page.basic.simple.title'),
+          icon: 'carbon:workspace',
+        },
+        name: 'BasicSimple',
+        path: 'simple',
+        component: () => import('#/views/basic/simple/simple.vue'),
+      },
+    ],
+  },
+];
+
+export default routes;
